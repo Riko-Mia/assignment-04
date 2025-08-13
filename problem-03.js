f1a903d5e78a5bf90cd23c0d692cb7eb67d782ea
Riko-Mia <rikomia722@gmail.com>
2025-08-13 23:16:55 +0600

    Problem 03 done

--- problem-03.js
+++ problem-03.js
@@ -1,5 +1,26 @@
+
+
+
 function  bestTeam( player1, player2 ) {
 
-          // You have to write your code here
+          let fare1 = player1.foul + player1.cardR + player1.cardY
+          let fare2 = player2.foul + player2.cardR + player2.cardY  
+
+          if(fare1 > fare2){
+            // console.log(player2.name)
+            return player2.name
+          } else if(fare1 < fare2){
+            // console.log(player1.name)
+            return player1.name
+          } else if(fare1 === fare2){
+            return "Tie"
+          }else{
+            return "Invalid"
+          }
 
 }
+
+
+console.log(bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },
+
+{ name: "France", foul: 10, cardY: 2, cardR: 1 }))
