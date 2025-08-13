0247666dbeefcd886e7b7eae743467fcd5c4a2ac
Riko Mia <rikomia722@gmail.com>
2025-08-14 02:20:41 +0600

    problem 4 start

---
+++ problem-04.js
@@ -0,0 +1,17 @@
+
+function  isSame(arr1 , arr2 ) {
+
+          // You have to write your code 
+          
+
+          if(arr1.length === arr2.length){
+            console.log('Array are same')
+            
+        }else{
+                  console.log('Array are not same')
+              }
+
+}
+
+isSame([1, 2, 3] , [1 , 2 , 3])
+
